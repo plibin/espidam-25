89b8d91202138842a0b533710d3e540117939f02
Pieter Libin <pieter.libin@vub.be>
2025-06-25 09:29:01 +0200

    add sir env

---
+++ epi-deep-rl/sir_env.py
@@ -0,0 +1,63 @@
+import numpy as np
+import gymnasium as gym
+from gymnasium import spaces
+from sir import initialise_modelstate, run_sir_model
+
+class SIREnv(gym.Env):
+    def __init__(self, budget, compartments, seeds, N_c, N_a, params):
+        self.budget = budget
+        self.seeds = seeds
+        self.N_c = N_c
+        self.N_a = N_a
+        self.N = N_c + N_a
+        self.params = params
+        self.compartments = compartments
+
+        # Define the action and observation space
+        self.action_space = gym.spaces.Discrete(2)
+        self.observation_space = spaces.Box(0, max(N_c, N_a), shape=(len(compartments),), dtype=np.float32)
+
+    # Return the current model state
+    def _get_obs(self):
+        #the np.maximum avoid that we get small negative numbers
+        return np.maximum(self.model_state,0)
+    
+    # This is not mandatory, but can be useful for debugging
+    def _get_info(self):
+        return {
+            "t": self._t,
+        }
+
+    # Reset the environment (modelstate, budget and time)
+    def reset(self, seed=None, options=None):
+        super().reset(seed=seed)
+
+    # Perform a step in the environment given an action
+    def step(self, action):
+    
+def make_sir_env(budget, seeds, N_c, N_a, gamma, beta):
+    # Register the environment
+    gym.envs.registration.register(
+        id="SIREnv-v0",
+        entry_point="sir_env:SIREnv",
+        max_episode_steps=300
+    )
+
+    compartments = ["S_c", "I_c", "R_c", "S_a", "I_a", "R_a"]
+    
+    params = {
+        "beta": beta, #transmission rate
+        "gamma": gamma, #recovery rate
+        "schools_closed": False 
+    }
+
+    #Make the environment
+    env = gym.make("SIREnv-v0",
+                    compartments=compartments,
+                    seeds=seeds,
+                    budget=budget,
+                    N_c=N_c,
+                    N_a=N_a,
+                    params=params)
+    
+    return env
