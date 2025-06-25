b6f56d58b78ce9ec9211f00b73b860d51e68b678
Pieter Libin <pieter.libin@vub.be>
2025-06-25 20:46:26 +0200

    solution

--- epi-deep-rl/sir_env.py
+++ epi-deep-rl/sir_env.py
@@ -31,9 +31,40 @@ class SIREnv(gym.Env):
     # Reset the environment (modelstate, budget and time)
     def reset(self, seed=None, options=None):
         super().reset(seed=seed)
+        self._t = 0
+        self.used_budget = 0
+        self.model_state = initialise_modelstate(self.seeds, self.N_c, self.N_a)
+        return self._get_obs(), self._get_info()
+
 
     # Perform a step in the environment given an action
     def step(self, action):
+        close_schools = (action == 1)
+
+        # If the budget is zero, we cannot close the schools
+        if self.used_budget >= self.budget:
+            close_schools = False
+
+        if close_schools:
+            self.used_budget += 1
+        self.params["schools_closed"] = close_schools
+            
+        end_t = self._get_info()["t"] + 7
+        
+        new_model_state = run_sir_model(self.model_state, end_t, self.params, [self.N_c, self.N_a])
+
+        _new_s = (new_model_state[self.compartments.index("S_c")]+new_model_state[self.compartments.index("S_a")]) 
+        _old_s = (self.model_state[self.compartments.index("S_c")]+self.model_state[self.compartments.index("S_a")])
+
+        self.model_state = new_model_state
+        self._t = end_t
+
+        # The reward is the new number of infected individuals, which is calculated as the difference between the old and new number of susceptible individuals
+        # Because it is a optimization problem, the reward is negative
+        reward = -(_old_s-_new_s)
+        terminated = self._t >= 180
+
+        return self._get_obs(), reward, terminated, False, self._get_info()
     
 def make_sir_env(budget, seeds, N_c, N_a, gamma, beta):
     # Register the environment
